f0628bc3b67004675b2ab7777450b2da8e24dbff
github-ci <you@example.com>
2025-05-25 08:22:12 +0000

    update

--- src/CouponWechatMiniProgramBundle.php
+++ src/CouponWechatMiniProgramBundle.php
@@ -3,7 +3,17 @@
 namespace Tourze\CouponWechatMiniProgramBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class CouponWechatMiniProgramBundle extends Bundle
+class CouponWechatMiniProgramBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Tourze\DoctrineUserBundle\DoctrineUserBundle::class => ['all' => true],
+            \Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle::class => ['all' => true],
+            \Tourze\DoctrineIpBundle\DoctrineIpBundle::class => ['all' => true],
+            \Tourze\DoctrineSnowflakeBundle\DoctrineSnowflakeBundle::class => ['all' => true],
+        ];
+    }
 }
